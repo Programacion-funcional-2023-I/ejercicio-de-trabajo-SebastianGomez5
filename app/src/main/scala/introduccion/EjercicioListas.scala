186ca09d9f711a1cd9030bffafafd8d2f2482d82
SebastianGomez5 <juansebastiancali8@gmail.com>
2023-08-29 19:18:04 -0500

    Solucion

--- app/src/main/scala/introduccion/EjercicioListas.scala
+++ app/src/main/scala/introduccion/EjercicioListas.scala
@@ -14,7 +14,20 @@ class EjercicioListas {
   def repetirListas(lista: List[Int], n: Int): List[List[Int]] = {
     var listaRepetida : List[List[Int]] = List()
     //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+    if (n >= 0){
+      var listaInterna = List()
+      for(elm <- lista){
+        var listaInterna: List[Int] = List()
+        for(i <- 1 to n){
+          listaInterna = listaInterna :+ elm
+        }
+        listaRepetida = listaRepetida :+ listaInterna
+      }
+      return listaRepetida
+    }
+    else{
+      throw new IllegalArgumentException("n debe ser mayor que cero")
+    }
   }
   /*
   * Punto 3: Filtrar listas
@@ -28,7 +41,44 @@ class EjercicioListas {
   def filtrarListas(criterioIn: String, n: Int, lista: List[Int]) : List[Int] = {
     var criterio : String = criterioIn.toLowerCase()
     var listaFiltrada : List[Int] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+
+    var funcion = (x: Int, y:Int) => x>y
+    if(criterio == "menor"){
+      funcion = (x: Int, y:Int) => x<y
+    }
+    else{
+      if(criterio == "mayoroigual"){
+        funcion = (x: Int, y:Int) => x>=y
+      }
+      else{
+        if(criterio == "menoroigual"){
+          funcion = (x: Int, y:Int) => x<=y
+        }
+        else{
+          if(criterio == "igual"){
+            funcion = (x: Int, y:Int) => x==y
+          }
+          else{
+            if(criterio == "diferente"){
+              funcion = (x: Int, y:Int) => x!=y
+            }
+            else{
+              if(criterio == "mayor"){
+                funcion = (x: Int, y:Int) => x>y
+              }
+              else{
+                throw new IllegalArgumentException("El criterio no es valido")
+              }
+            }
+          }
+        }
+      }
+    }
+    for(elm <- lista){
+      if(funcion(elm, n)){
+        listaFiltrada = listaFiltrada :+ elm
+      }
+    }
+    return listaFiltrada
   }
 }
